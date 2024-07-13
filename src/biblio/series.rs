3648fa3f9d3488d1642143808b47ee770e71f3d7
ChousX <gmanenergy@gmail.com>
2024-07-13 11:06:10 -0400

    impl most of the file logic

--- src/biblio/series.rs
+++ src/biblio/series.rs
@@ -8,7 +8,7 @@ pub struct Series{
 
 impl Series {
     pub fn get(&self, title: &str) -> Option<Entity> {
-        self.data.get(title)
+        self.data.get(title).copied()
     }
 
     pub fn add(&mut self, title: String, id: Entity) {
@@ -33,6 +33,12 @@ pub struct SeriesBook{
     pub id: Entity,
     pub book_number: u16,
 }
+impl SeriesBook {
+    pub fn new(id: Entity, book_number: usize) -> Self{
+        let book_number = book_number as u16;
+        Self { id, book_number}
+    }
+}
 
 pub fn add_series(
     mut commands: Commands,
@@ -46,18 +52,18 @@ pub fn add_series(
             if let Ok(mut works) = series_works.get_mut(id){
                 let len = works.0.len(); 
                 works.0.push(entity);
-                commands.entity(entity).insert(SeriesBook(id, len));
+                commands.entity(entity).insert(SeriesBook::new(id, len));
             }
         } else {
             let series_works = SeriesWorks(vec![entity]);
             let id = commands.spawn(
                 (
                     series_works,
-                    Title::new(title),
+                    Title(title.to_string()),
                 
             )).id();
-            series.add(title, id);
-            commands.entity(entity).insert(SeriesBook(id, 0));
+            series.add(title.to_string(), id);
+            commands.entity(entity).insert(SeriesBook::new(id, 0));
         }
     }
 }
--- src/files.rs
+++ src/files.rs
@@ -1,3 +1,7 @@
+use bevy::scene::ron::{to_string, Result};
+use std::path::{Path, PathBuf};
+use std::fs;
+use std::collections::BinaryHeap as Heap;
 use crate::prelude::*;
 
 pub const DEFAULT_INPUT_FOLDER: &str = "/media/Void/pre-books";
@@ -9,21 +13,23 @@ impl Plugin for FilesPlugin {
         app
             .init_resource::<StorageFolder>()
             .init_resource::<InputsFolders>()
+            .add_event::<AddNewBooksEvent>()
+            .add_event::<MoveBookEvent>()
+            .add_event::<FixSingleFiles>()
             .add_systems(
-                Update,
-                add_dir_inputs.run_if(on_event::<AddNewBooksEvent>())
+                PreUpdate,
+                    add_dir_inputs.run_if(on_event::<AddNewBooksEvent>()),
             ).add_systems(
-                Update, 
+                Update,
                 (
-                    //I hope this works
-                    check_folders.after(add_dir_inputs),
-                    check_folders.befor(move_book_to_storage)
+                     move_book_to_storage,
+                     fix_single_files.run_if(on_event::<FixSingleFiles>()),
                 )
             );
     }
 }
 
-#[derive(Resource, Default, Deref, DerefMut)]
+#[derive(Resource, Deref, DerefMut)]
 pub struct InputsFolders(Vec<String>);
 impl Default for InputsFolders {
     fn default() -> Self {
@@ -39,6 +45,12 @@ impl Default for StorageFolder {
     }
 }
 
+impl StorageFolder {
+    pub fn path<'a>(&'a self) -> &'a Path{
+        Path::new(&self.0)
+    }
+}
+
 #[derive(Component, Deref, DerefMut)]
 pub struct DataFolder(pub String);
 
@@ -48,47 +60,86 @@ pub struct UnmovedData;
 #[derive(Event, Default)]
 pub struct AddNewBooksEvent;
 
-fn add_dir_inputs(
-    mut commands: Commands,
-    input_folders: Res<InputsFolders>,
-) {
-    for folder in *input_folders.iter(){
-        let dir_path = Path::new(folder);
-        if let Ok(dir_iter) = std::fs::read_dir(dir_path){
-            for book in dir_iter {
-                if let Ok(book) = book {
-                    commands
-                        .spawn((DataFolder(book.path().into()), UnmovedData));
-                }
+
+struct FoldersInfo{
+    pub folders_with_sub_folders: Vec<String>,
+    pub folders_with_no_sub_folders: Vec<String>,
+    pub loss_files: Vec<String>,
+}
+
+impl FoldersInfo {
+    pub fn new(
+        root_folder: &str,
+    ) -> Result<Self>{
+    
+        let root_path = Path::new(root_folder);
+        let mut folder_iter = std::fs::read_dir(root_path)?;
+        let mut queue = Heap::new();
+    
+        let mut loss_files = Vec::new();
+        for path in folder_iter{
+            let path = path?.path();
+            let path_str = path.clone().into_os_string().into_string().expect("bad file path");
+            if path.is_dir() {
+                queue.push(path_str);
+            } else {
+                loss_files.push(path_str);
             }
         }
+
+        let mut folders_with_no_sub_folders = Vec::new();
+        let mut folders_with_sub_folders = Vec::new();
+
+        while let Some(subject) = queue.pop() {
+            let subject_path = Path::new(&subject);
+
+            let mut seen_folder = false;
+            let mut folder_iter = std::fs::read_dir(subject_path)?;
+            for child in folder_iter {
+                let child = child?;
+                let child_path = child.path();
+                if child_path.is_dir() {
+                    seen_folder = true;
+                    let child_str = child_path.into_os_string().into_string().expect("bad file path str");
+                    queue.push(child_str);
+                } 
+            }
+
+            if seen_folder {
+                folders_with_sub_folders.push(subject);
+            } else {
+                folders_with_no_sub_folders.push(subject)
+            }
+        }
+        Ok(Self {
+            loss_files,
+            folders_with_no_sub_folders,
+            folders_with_sub_folders,
+        })
     }
 }
 
 
-#[derive(Event, Default)]
-pub struct MoveBookEvent(pub Entity);
-
-fn check_folders(
+fn add_dir_inputs(
     mut commands: Commands,
-    mut files: Query<(&DataFolder, Entity), With<UnmovedData>>,
+    input_folders: Res<InputsFolders>,
 ) {
-    let mut books = Vec::new();
-    for (dir_path, entity) in files.iter() {
-        if let Ok(file_iter) = std::fs::read_dir(dir_path) {
-            for DataFolder(entry) in file_iter {
-                if let Ok(path) = entry.path(){
-                    if path.is_dir() {
-                        books.push(path)
-                    } else {}
-                }
-            }
-        }
-        if !books.is_empty() {
-            commands.entity(entity).despawn();
-            for new_book in books.drain(){
+    for folder in input_folders.0.iter(){
+        if let Ok(FoldersInfo { 
+            folders_with_sub_folders,
+            mut folders_with_no_sub_folders, 
+            mut loss_files 
+        }) = FoldersInfo::new(folder){
+            for book in loss_files.into_iter(){
                 commands.spawn((
-                    DataFolder(new_book.into()),
+                    DataFolder(book),
+                    UnmovedData,
+                    SingleFile,
+                ));
+            }
+            for book in folders_with_no_sub_folders.into_iter(){
+                commands.spawn((
+                    DataFolder(book),
                     UnmovedData,
                 ));
             }
@@ -96,20 +147,117 @@ fn check_folders(
     }
 }
 
+#[derive(Component)]
+pub struct SingleFile;
+
+#[derive(Event )]
+pub struct FixSingleFiles;
+
+fn fix_single_files(
+    mut loss_files: Query<(&mut DataFolder, Entity), With<SingleFile>>
+    , mut commands: Commands,
+){
+    for (mut file, entity) in loss_files.iter_mut(){
+        let file_path = Path::new(&file.0);
+        let new_folder_path = {
+            let file_name = file_path.file_stem().unwrap();
+            let mut file_path = file_path.to_path_buf();
+            file_path.pop();
+            file_path.push(file_name);
+            file_path
+        };
+
+        if let Err(err) = copy(&file_path, &new_folder_path){
+            panic!("{}", err);
+        }
+
+        commands.entity(entity).remove::<SingleFile>();
+
+    }
+}
+
+#[derive(Event )]
+pub struct MoveBookEvent(pub Entity);
+
 fn move_book_to_storage(
-    mut file_dir: Query<&DataFolde, With<UnmovedData>>,
+    mut file_dir: Query<(&mut DataFolder, &Title), With<UnmovedData>>,
     mut events: EventReader<MoveBookEvent>,
     mut commands: Commands,
-    storage_dir: Res<StorageFolder>,
+    storage: Res<StorageFolder>,
 ) {
-    for MoveBookEvent(book_id) in events.iter(){
-        if let Ok(book_current_dir) = file_dir.get(book_id){
-            //Do stuff to move book over to storage
-            //After moved remove eg: 
-            //  commands.entity(book_id).remove_component::<UnmovedData>();
-            todo!()
+    let storage = storage.path();
+    for MoveBookEvent(book_id) in events.read(){
+        if let Ok(( mut book_current_dir, Title(title))) = file_dir.get_mut(*book_id){
+            let to = {
+                let mut temp = storage.to_path_buf();
+                temp.push(title);
+                temp
+            };
+
+            let from = Path::new(&book_current_dir.0);
+            if from.is_file() {
+                panic!("Somethings has gone wrong")
+            }
+            if let Err(err) = copy(&from, &to){
+                //Lets see this err!
+                panic!("{}", err);
+            }
+        //Folder has been moved
+            //Updating folder Local
+            book_current_dir.0 = to.into_os_string().into_string().expect("bad file str");;
+            
+            //removing unmoved_data tag;
+            commands.entity(*book_id).remove::<UnmovedData>();
         } else {
+            //this should never happen
             panic!("missing book.... not sure how");
         }
     }
+}
+
+pub fn copy<U: AsRef<Path>, V: AsRef<Path>>(from: U, to: V) -> Result<(), std::io::Error> {
+    let mut stack = Vec::new();
+    stack.push(PathBuf::from(from.as_ref()));
+
+    let output_root = PathBuf::from(to.as_ref());
+    let input_root = PathBuf::from(from.as_ref()).components().count();
+
+    while let Some(working_path) = stack.pop() {
+        println!("process: {:?}", &working_path);
+
+        // Generate a relative path
+        let src: PathBuf = working_path.components().skip(input_root).collect();
+
+        // Create a destination if missing
+        let dest = if src.components().count() == 0 {
+            output_root.clone()
+        } else {
+            output_root.join(&src)
+        };
+        if fs::metadata(&dest).is_err() {
+            println!(" mkdir: {:?}", dest);
+            fs::create_dir_all(&dest)?;
+        }
+
+        for entry in fs::read_dir(working_path)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                stack.push(path);
+            } else {
+                match path.file_name() {
+                    Some(filename) => {
+                        let dest_path = dest.join(filename);
+                        println!("  copy: {:?} -> {:?}", &path, &dest_path);
+                        fs::copy(&path, &dest_path)?;
+                    }
+                    None => {
+                        println!("failed: {:?}", path);
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(())
 }
