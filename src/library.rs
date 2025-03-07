dc6b88162d716c8c912aa297fad85b4c82a17d7d
Allen Garrett Gelwick III <gmanenergy@gmail.com>
2025-03-06 22:11:02 -0500

    feat: add audiotags dependency and enhance book metadata extraction

--- src/library.rs
+++ src/library.rs
@@ -7,6 +7,8 @@ use std::{
 
 use nfo::*;
 
+use audiotags as AT;
+
 use bevy::{prelude::*, utils::HashMap};
 
 pub struct LibraryPlugin;
@@ -14,8 +16,13 @@ impl Plugin for LibraryPlugin {
     fn build(&self, app: &mut App) {}
 }
 
-pub fn add_book(mut commmands: Commands, query: Query<&PathToBook>) {
-    for PathToBook(path) in query.iter() {
+pub fn add_book(
+    mut commmands: Commands,
+    query: Query<(&PathToBook, Entity)>,
+    mut authors: ResMut<Authors>,
+    mut narators: ResMut<Narators>,
+) {
+    for (PathToBook(path), entity) in query.iter() {
         let path = Path::new(&path);
         let mut files = if path.is_file() {
             let p = PathBuf::from(path);
@@ -56,14 +63,6 @@ pub fn add_book(mut commmands: Commands, query: Query<&PathToBook>) {
         let mut author = None;
         let mut narator = None;
 
-        fn is_more_work(
-            title: Option<String>,
-            author: Option<String>,
-            narator: Option<String>,
-        ) -> bool {
-            title.is_none() || author.is_none() || narator.is_none()
-        }
-
         if let Some(index) = files
             .iter()
             .position(|(extention, _file)| extention == "nfo")
@@ -75,7 +74,53 @@ pub fn add_book(mut commmands: Commands, query: Query<&PathToBook>) {
             narator = nfo.general.read_by;
         }
 
-        if is_more_work(title, author, narator) {}
+        if title.is_none() || author.is_none() || narator.is_none() {
+            if let Some(index) = files
+                .iter()
+                .position(|(extention, _file)| extention == "m4b")
+            {
+                let (_extention, m4b_path) = &files[index];
+                let tag = AT::Tag::new()
+                    .read_from_path(m4b_path)
+                    .expect("Could not parse tag");
+                let album = tag.album().expect("failed to get album");
+                if title.is_none() {
+                    title = Some(album.title.to_string());
+                }
+                if author.is_none() {
+                    author = album.artist.map(|s| s.to_string());
+                }
+            }
+        }
+
+        if title.is_none() || author.is_none() {
+            if let Some(index) = files
+                .iter()
+                .position(|(extention, _file)| extention == "mp3")
+            {
+                let (_extention, m4b_path) = &files[index];
+                let tag = AT::Tag::new()
+                    .read_from_path(m4b_path)
+                    .expect("Could not parse tag");
+                let album = tag.album().expect("failed to get album");
+                if title.is_none() {
+                    title = Some(album.title.to_string());
+                }
+                if author.is_none() {
+                    author = album.artist.map(|s| s.to_string());
+                }
+            }
+        }
+        let mut commands = commmands.entity(entity);
+        if let Some(title) = title {
+            commands.insert(Title(title));
+        }
+        if let Some(author) = author {
+            commands.insert(authors.get_author(&author));
+        }
+        if let Some(narator) = narator {
+            commands.insert(narators.get_narator(&narator));
+        }
     }
 }
 
@@ -88,8 +133,70 @@ pub struct Title(pub String);
 #[derive(Resource)]
 pub struct Series;
 
-#[derive(Resource)]
-pub struct Author;
+#[derive(Resource, Default)]
+pub struct Authors {
+    entries: Vec<String>,
+    indices: HashMap<String, usize>,
+}
+
+impl Authors {
+    pub fn get_author(&mut self, author: &str) -> Author {
+        Author(self.get_or_insert(author))
+    }
+
+    fn get_or_insert(&mut self, author: &str) -> usize {
+        if let Some(&index) = self.indices.get(author) {
+            index
+        } else {
+            let index = self.entries.len();
+            self.entries.push(author.to_string());
+            self.indices.insert(author.to_string(), index);
+            index
+        }
+    }
+
+    pub fn get(&self, index: usize) -> Option<&str> {
+        self.entries.get(index).map(|s| s.as_str())
+    }
+
+    pub fn get_index(&self, author: &str) -> Option<usize> {
+        self.indices.get(author).copied()
+    }
+}
+
+#[derive(Component)]
+pub struct Author(pub usize);
 
 #[derive(Resource)]
-pub struct Narator;
+pub struct Narators {
+    entries: Vec<String>,
+    indices: HashMap<String, usize>,
+}
+
+impl Narators {
+    pub fn get_narator(&mut self, narator: &str) -> Narator {
+        Narator(self.get_or_insert(narator))
+    }
+
+    fn get_or_insert(&mut self, narator: &str) -> usize {
+        if let Some(&index) = self.indices.get(narator) {
+            index
+        } else {
+            let index = self.entries.len();
+            self.entries.push(narator.to_string());
+            self.indices.insert(narator.to_string(), index);
+            index
+        }
+    }
+
+    pub fn get(&self, index: usize) -> Option<&str> {
+        self.entries.get(index).map(|s| s.as_str())
+    }
+
+    pub fn get_index(&self, author: &str) -> Option<usize> {
+        self.indices.get(author).copied()
+    }
+}
+
+#[derive(Component)]
+pub struct Narator(pub usize);
